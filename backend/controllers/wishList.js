4f6a503133fca1e2b48e9e117b610a02d54c1f7b
FatimaKilani <fatima.keelani@gmail.com>
2022-05-11 23:52:20 +0300

    Delete Wish List By Id function (Done)

--- backend/controllers/wishList.js
+++ backend/controllers/wishList.js
@@ -68,4 +68,30 @@ const getAllWishLists = (req, res) => {
     });
 };
 
-module.exports = { createWishList, getAllWishLists };
+// This function deletes a specific article by its id
+const deleteWishListById = (req, res) => {
+  const id = req.params.id;
+  wishListModel
+    .findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The wishList: ${id} is not found`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `WishList deleted`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+module.exports = { createWishList, getAllWishLists, deleteWishListById };
