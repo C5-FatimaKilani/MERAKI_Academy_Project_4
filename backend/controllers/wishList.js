76a430ddaa16b25630c8e99e310a71c8a7881cc2
FatimaKilani <fatima.keelani@gmail.com>
2022-05-11 23:38:11 +0300

    Get Wish list function (Done)

--- backend/controllers/wishList.js
+++ backend/controllers/wishList.js
@@ -12,31 +12,60 @@ const createWishList = (req, res) => {
   newWishList
     .save()
     .then((result) => {
-        wishListModel
-          .updateOne({ _id: productId }, { $push: { wishLists: result._id } })
-          .then(() => {
-            res.status(201).json({
-              success: true,
-              message: `wishList added`,
-              wishLists: result,
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              success: false,
-              message: `Server Error`,
-              err: err.message,
-            });
+      wishListModel
+        .updateOne({ _id: productId }, { $push: { wishLists: result._id } })
+        .then(() => {
+          res.status(201).json({
+            success: true,
+            message: `wishList added`,
+            wishLists: result,
           });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: `Server Error`,
-          err: err.message,
+        })
+        .catch((err) => {
+          res.status(500).json({
+            success: false,
+            message: `Server Error`,
+            err: err.message,
+          });
         });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
       });
-  };
-  
+    });
+};
 
-module.exports = { createWishList };
+// This function returns the wishLists
+const getAllWishLists = (req, res) => {
+  const userId = req.token.userId;
+  wishListModel
+    .find({})
+    // .populate("comments")
+    .then((wishLists) => {
+      if (wishLists.length) {
+        res.status(200).json({
+          success: true,
+          message: `All the wishLists`,
+          userId: userId,
+          wishLists: wishLists,
+        });
+      } else {
+        res.status(200).json({
+          success: false,
+          message: `No wishLists Yet`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+module.exports = { createWishList, getAllWishLists };
