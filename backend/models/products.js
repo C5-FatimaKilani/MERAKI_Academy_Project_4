b31ff3fe053e0156037996427f91e202471e7305
FatimaKilani <fatima.keelani@gmail.com>
2022-05-11 23:56:01 +0300

    add  (required: true) to img in product schema (Done)

--- backend/models/products.js
+++ backend/models/products.js
@@ -6,8 +6,9 @@ const products = new mongoose.Schema({
   price: { type: Number },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-img:{type:String},
+img:{type:String , required: true},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  wishLists: [{type: mongoose.Schema.Types.ObjectId, ref: "WishList"}]
 });
 
 module.exports = mongoose.model("Product", products);
