3fb60c61ab24ad86faef2ef5f1856bee50d2ff6c
FatimaKilani <fatima.keelani@gmail.com>
2022-05-12 22:25:50 +0300

    Login component (Done)

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, {  useState, useContext } from "react";
+import axios from "axios";
+import { methodContext } from "../../App";
+
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { token, setToken, message, setMessage, isLoggedIn, setIsLoggedIn } =
+    useContext(methodContext);
+
+    const clickLogin = (e) => {
+      // prevent the form from refreshing the whole page
+      e.preventDefault();
+  
+      axios
+        .post("http://localhost:5000/login/", {
+          email: email,
+          password: password,
+        })
+        .then((result) => {
+          setToken(result.data.token);
+          setIsLoggedIn(true);
+          navigate("/dashboard");
+          localStorage.setItem("token",result.data.token)
+        })
+        .catch((error) => {
+          console.log(error.response.data.message);
+          setIsLoggedIn(false);
+          setMessage(error.response.data.message);
+        });
+    };
   
   return (
-    <div>Login</div>
+    <div>Login
+       <input
+        type="email"
+        placeholder="Email "
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="password"
+        placeholder="Password "
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={clickLogin}>Login</button>
+      <br />
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
+    </div>
   )
 }
 
